1ea64219fd7841d15114c19cd88532ca9f5664a4
Klaudio22 <47950499+Klaudio22@users.noreply.github.com>
2021-12-05 19:22:13 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname));
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(3333);
+
+console.log("Now the server is running in url: http://127.0.0.1:3333")
